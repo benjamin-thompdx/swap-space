88b364a8f7bd8618b73fedec5c582827e7b85252
Benjamin Thom <jaminthom@gmail.com>
2020-05-18 11:23:44 -0700

    update src/App.js to include basic layout and navigation menu with Bootstrap

--- src/App.js
+++ src/App.js
@@ -11,11 +11,28 @@ class App extends Component {
     return (
       <Router>
         <div className='container'>
-          <h2>Swap Space</h2>
+
+          <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <a className='navbar-brand' href='https://benjaminthompdx.wordpress.com/' target='blank'>
+              <img src={logo} width='120' height='120' alt='benjaminthompdx.wordpress.com' />
+            </a>
+            <Link to='/' className='navbar-brand'>Swap Space</Link>
+            <div className='collapse navbar-collapse'>
+              <ul className='navbar-nav ml-auto'>
+                <li className='navbar-item'>
+                  <Link to='/' className='nav-link'>Items</Link>
+                </li>
+                <li className='navbar-item'>
+                  <Link to='/create' className='nav-link'>Create Item</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path='/' exact component={ItemList} />
+          <Route path='/edit/:id' component={EditItem} />
+          <Route path='/create' component={CreateItem} />
         </div> 
-        <Route path='/' exact component={ItemList} />
-        <Route path='/edit/:id' component={EditItem} />
-        <Route path='/create' component={CreateItem} />
       </Router>
     );
   }
