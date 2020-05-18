788f49122f190d7e27d1bd7342e3b02e7f22d8a3
Benjamin Thom <jaminthom@gmail.com>
2020-05-18 09:46:03 -0700

    update src/App.js to include an import statement for react-router-dom, implement Router configuration at the top level of the render return statement, create routes for home directory, edit, and create components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-
-      <div className="container">
-        <h2>Swap Space</h2>
-      </div>
-
+      <Router>
+        <div className='container'>
+          <h2>Swap Space</h2>
+        </div> 
+        <Route path='/' exact component={ItemList} />
+        <Route path='/edit/:id' component={EditItem} />
+        <Route path='/create' component={CreateItem} />
+      </Router>
     );
   }
 }
