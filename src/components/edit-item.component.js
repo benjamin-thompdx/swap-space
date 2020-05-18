552082bbc72f6373e70e4a8ed7ba6886682ca266
Benjamin Thom <jaminthom@gmail.com>
2020-05-18 09:52:56 -0700

    update src/components/edit-item.component.js to include boilerplate code

---
+++ src/components/edit-item.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class EditItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>Edit Item</p>
+      </div>
+    )
+  }
+}
