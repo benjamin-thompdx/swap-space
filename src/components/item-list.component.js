d567b004d151846942b27cc17d01656eee770766
Benjamin Thom <jaminthom@gmail.com>
2020-05-18 09:51:10 -0700

    update src/components/item-list.component.js to include boilerplate code

---
+++ src/components/item-list.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class ItemList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Trade Item List</p>
+      </div>
+    )
+  }
+} 
