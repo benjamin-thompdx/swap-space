5e45adf7b7797bc0598b2148a162331eebc77262
Benjamin Thom <jaminthom@gmail.com>
2020-05-19 10:05:08 -0700

    revert changes made to backend/server.js sytax to utilize Nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-import { json } from 'body-parser';
-import cors from 'cors';
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const PORT = 4000;
 
 app.use(cors());
-app.use(json());
+app.use(bodyParser.json());
 
 app.listen(PORT, function() {
-  console.log('Server is running on Port: ' + PORT);
+  console.log("Server is running on Port: " + PORT);
 });
