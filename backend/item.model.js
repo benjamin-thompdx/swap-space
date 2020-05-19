0856eca8838e9547e9777dbbf39a56b65c3b6b1d
Benjamin Thom <jaminthom@gmail.com>
2020-05-19 11:24:55 -0700

    update backend/item.model.js to include 'Item' schema for our 'Items' database

---
+++ backend/item.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoos');
+const Schema = mongoose.Schema;
+
+let Item = new Schema({
+  item_name: {
+    type: String
+  },
+  item_description: {
+    type: String
+  },
+  item_image: {
+    type: String
+  },
+  item_zipCode: {
+    type: String
+  },
+  item_value: {
+    type: String
+  },
+  item_traded: {
+    type: Boolean
+  }
+});
+
+module.exports = mongoose.model('Item', Item);
