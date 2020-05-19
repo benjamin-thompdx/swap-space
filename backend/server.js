65f85e4708e2bae84472d788a7526e21dbb7c366
Benjamin Thom <jaminthom@gmail.com>
2020-05-19 09:59:37 -0700

    update sytax within backend/server.js to match ES6 syntax

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import { json } from 'body-parser';
+import cors from 'cors';
 const PORT = 4000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(json());
 
 app.listen(PORT, function() {
-  console.log("Server is running on Port: " + PORT);
+  console.log('Server is running on Port: ' + PORT);
 });
