2fdf9fc0c9cbb8d928cd764cd9857bf25142a93d
Benjamin Thom <jaminthom@gmail.com>
2020-05-20 16:52:14 -0700

    update project structure, update src/components/item-list.component.jsto include a table to display item values and a link to edit edit items

--- src/components/item-list.component.js
+++ src/components/item-list.component.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+const Item = props => (
+  <tr>
+    <td>props.item.item_name</td>
+    <td>props.item.item_description</td>
+    <td>props.item.item_image</td>
+    <td>props.item.item_zipCode</td>
+    <td>props.item.item_value</td>
+    <td>
+      <Link to={'/edit/'+props.item_id}>Edit Item</Link>
+    </td>
+  </tr>
+)
 
 export default class ItemList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {items: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:4000/items/')
+      .then(response => {
+        this.setState({items: response.data});
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+  itemList() {
+    return this.state.items.map(function(currentItem, i) {
+      return <Item item={currentItem} key={i} />;
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>Item List</p>
+        <h3>Item List</h3>
+        <table className='table table-striped' style={{ marginTop: 20 }}>
+          <thread>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Image</th>
+              <th>Zip Code</th>
+              <th>Value</th>
+              <th>Actions</th>
+            </tr>
+          </thread>
+          <tbody>
+            { this.itemList() }
+          </tbody>
+        </table>
       </div>
     )
   }
---
+++ swap-space-backend/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const itemRoutes = express.Router();
+const PORT = 4000;
+
+let Item = require('./item.model');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/items', { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+  console.log('MongoDb database connection established successfully');
+})
+
+itemRoutes.route('/').get(function(req, res) {
+  Item.find(function(err, items) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(items);
+    }
+  });
+});
+
+itemRoutes.route('/:id').get(function(req, res) {
+  let id = req.params.id;
+  Item.findById(id, function(err, item) {
+    res.json(item);
+  });
+});
+
+itemRoutes.route('/add').post(function(req, res) {
+  let item = new Item(req.body);
+  item.save()
+      .then(item => {
+        res.status(200).json({'item': 'item added successfully'});
+      })
+      .catch(err => {
+        res.status(400).send('adding new item failed');
+      });
+});
+
+itemRoutes.route('/update/:id').post(function(req, res) {
+  Item.findById(req.params.id, function(err, item) {
+    if (!item)
+      res.status(404).send('data is not found');
+    else
+      item.item_name = req.body.item_name;
+      item.item_description = req.body.item_description;
+      item.item_image = req.body.item_image;
+      item.item_zipCode = req.body.item_zipCode;
+      item.item_value = req.body.item_value;
+      item.item_traded = req.body.item_traded;
+
+      item.save().then(item => {
+        res.json('Item updated');
+      })
+      .catch(err => {
+        res.status(400).send("Update not possible");
+      });
+  });
+});
+
+app.use('/items', itemRoutes);
+
+app.listen(PORT, function() {
+  console.log("Server is running on Port: " + PORT);
+});
